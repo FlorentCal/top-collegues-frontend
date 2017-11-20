3d6de700eca7c43baf610470c8ed51671de0c999
Florent Callaou <florent.callaou22@gmail.com>
2017-11-20 11:38:49 +0100

    deploy

--- src/app/shared/service/collegue.service.ts
+++ src/app/shared/service/collegue.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Collegue } from '../domain/collegue';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment'
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -15,28 +16,28 @@ export class CollegueService {
 
   ]
 
-  constructor(private http: HttpClient) { 
-    
+  constructor(private http: HttpClient) {
+
   }
 
   listerCollegues(): Promise<Collegue[]> {
-    return this.http.get<Collegue[]>('http://localhost:8080/collegues').toPromise()
+    return this.http.get<Collegue[]>(environment.apiUrl + '/collegues').toPromise()
   }
 
   trouverCollegue(nom: string): Promise<Collegue> {
-    return this.http.get<Collegue>(`http://localhost:8080/collegues/detail/${nom}`).toPromise()
+    return this.http.get<Collegue>(environment.apiUrl + `/collegues/detail/${nom}`).toPromise()
   }
 
   sauvegarder(newCollegue: Collegue): Promise<Collegue> {
-    return this.http.post<Collegue>('http://localhost:8080/collegues', newCollegue, httpOptions).toPromise()
+    return this.http.post<Collegue>(environment.apiUrl + '/collegues', newCollegue, httpOptions).toPromise()
   }
 
   aimerUnCollegue(unCollegue: Collegue): Promise<Collegue> {
-    return this.http.put<Collegue>('http://localhost:8080/collegues/'+ unCollegue.nom + '/score', 10, httpOptions).toPromise()
+    return this.http.put<Collegue>(environment.apiUrl + '/collegues/' + unCollegue.nom + '/score', 10, httpOptions).toPromise()
   }
 
   detesterUnCollegue(unCollegue: Collegue): Promise<Collegue> {
-    return this.http.put<Collegue>('http://localhost:8080/collegues/'+ unCollegue.nom + '/score', -5, httpOptions).toPromise()
+    return this.http.put<Collegue>(environment.apiUrl + '/collegues/' + unCollegue.nom + '/score', -5, httpOptions).toPromise()
   }
 
 
